4c9d86cbda7606c0fd9eec5cada68953e1784272
Raza Balbale <ribalbale@gmail.com>
2024-01-09 23:07:39 -0600

    Added help text on failed invocation.

--- gmx/main.py
+++ gmx/main.py
@@ -6,5 +6,15 @@ app = typer.Typer()
 app.add_typer(workflow_app, name="wf")
 app.add_typer(project_app, name="proj")
 
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    """
+    Generate code using gmx.
+    """
+    if ctx.invoked_subcommand is None:
+        typer.echo("Welcome to Genmax!!")
+        typer.echo("To explore commands at any time, type:")
+        typer.echo("gmx --help")
+
 if __name__ == "__main__":
     app()
