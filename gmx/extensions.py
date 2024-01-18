0d6cc1351e87228b62e886c85978edaa0c816884
Raza Balbale <ribalbale@gmail.com>
2024-01-17 22:52:12 -0600

    Added support for env file read.

--- gmx/extensions.py
+++ gmx/extensions.py
@@ -17,3 +17,18 @@ def path(x): return caseswitcher.to_path(x)
 def uuid(): return str(uuid.uuid4())
 def secret(): return secrets.token_hex(16)
 def secret_complex(): return secrets.token_hex(64)
+def env(env_file_name, config_key):
+    try:
+        with open(f'{env_file_name}.env', 'r') as file:
+            for line in file:
+                # Splitting each line by '=' and stripping whitespace
+                key_value = line.strip().split('=', 1)
+                if len(key_value) == 2:
+                    key, value = key_value
+                    if key.strip() == config_key:
+                        return value.strip()
+    except FileNotFoundError:
+        print(f"The file {env_file_name}.env was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return None
