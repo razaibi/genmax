6598a4d23063515ff093b5830e3760ed361d5cf5
Raza Balbale <ribalbale@gmail.com>
2024-01-17 22:54:33 -0600

    Updated version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='genmax',
-    version='0.0.9',
+    version='0.1.1',
     author='Raza Balbale',
     description='A simple code generation tool.',
     long_description=long_description,
